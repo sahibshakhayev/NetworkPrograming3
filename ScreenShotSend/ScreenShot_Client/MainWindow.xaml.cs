82deaedaa6aa6b98fa247a64ef9b077aa4d265b1
Sahib Shakhayev <sahib.shakhayev@hotmail.com>
2023-04-09 00:10:51 +0400

    Add files via upload

---
+++ ScreenShotSend/ScreenShot_Client/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ScreenShot_Client
+{
+
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly UdpClient _udpClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _udpClient = new UdpClient();
+        }
+
+        private void GetSCBt_Click(object sender)
+        {
+        
+        
+    }
+
+        private  async void GetSCBt_Click_1(object sender, RoutedEventArgs e)
+        {
+            var serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27001);
+
+
+            var commandData = Encoding.ASCII.GetBytes("screenshot");
+
+            await _udpClient.SendAsync(commandData, commandData.Length, serverEndpoint);
+
+            var imageData = await ReceiveData();
+            var imageSource = ConvertToImageSource(imageData);
+            Screenshot.Source = imageSource;
+        }
+
+        private async Task<byte[]> ReceiveData()
+        {
+            const int bufferSize = 1024;
+            var buffer = new byte[bufferSize];
+            var receivedData = new byte[0];
+
+            while (true)
+            {
+                var result = await _udpClient.ReceiveAsync();
+                var packetData = result.Buffer;
+                receivedData = receivedData.Concat(packetData).ToArray();
+                if (packetData.Length < bufferSize)
+                {
+                    break;
+                }
+            }
+
+            return receivedData;
+        }
+
+        private BitmapImage ConvertToImageSource(byte[] imageData)
+        {
+            using (var memoryStream = new MemoryStream(imageData))
+            {
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+
+
+
+
+    }
+    }
+
---
+++ ScreenShotSend/ScreenShot_Server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+
+
+
+
+ var _udpClient = new UdpClient(27001);
+var _remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+Console.WriteLine("Screenshot Server Started...");
+
+while (true)
+{
+    Console.WriteLine("Waiting Conection...");
+    var data = _udpClient.Receive(ref _remoteEndPoint);
+    var command = Encoding.ASCII.GetString(data);
+
+    if (command == "screenshot")
+    {
+        var screenshotData = GetScreenshot();
+        Console.WriteLine($"Screenshot Requested from IP: {_remoteEndPoint.Address}");
+        SendData(screenshotData);
+    }
+}
+
+
+static byte[] GetScreenshot()
+{
+
+    const int SM_CXSCREEN = 0;
+    const int SM_CYSCREEN = 1;
+
+    var screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    var screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+
+    using (var bitmap = new Bitmap(screenWidth,screenHeight))
+    {
+        using (var g = Graphics.FromImage(bitmap))
+        {
+            g.CopyFromScreen(Point.Empty, Point.Empty, new Size(screenWidth,screenHeight));
+        }
+        using (var memoryStream = new MemoryStream())
+        {
+            bitmap.Save(memoryStream, ImageFormat.Jpeg);
+            return memoryStream.ToArray();
+        }
+    }
+}
+
+void SendData(byte[] data)
+{
+    const int bufferSize = 1024;
+    var totalPackets = (int)Math.Ceiling((double)data.Length / bufferSize);
+
+    for (var i = 0; i < totalPackets; i++)
+    {
+        var packetSize = Math.Min(bufferSize, data.Length - i * bufferSize);
+        var packetData = new byte[packetSize];
+        Array.Copy(data, i * bufferSize, packetData, 0, packetSize);
+        _udpClient.Send(packetData, packetSize, _remoteEndPoint);
+        Thread.Sleep(10); 
+    }
+}
+
+[DllImport("user32.dll")]
+static extern int GetSystemMetrics(int nIndex);
+
+
+
+
+
+
+
+
+
+
